b62540cf12e831f2295482f1c4e3665dd70585a2
Prima Aulia Gusta <primaulia@gmail.com>
2021-09-08 15:13:59 +0800

    latest with form creation

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -6,4 +6,42 @@ class CustomersController < ApplicationController
   def show
     @customer = Customer.find(params[:id])
   end
+
+  def new
+    # a blank instance of a customer
+    @new_customer = Customer.new
+    # pass something to the view
+  end
+
+  def create
+    new_customer = Customer.new(customer_params)
+
+    if new_customer.save
+      redirect_to root_path
+    else
+      
+    end
+
+    # raise
+
+    # extract the data out of params
+    # create new instance of customer
+    # save it
+    # redirected to another route
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :address)
+  end
+
+
+
 end
+
+# {
+#   "authenticity_token"=>"wPQQpqjDSQs3u5Nk5Ab2kZgwqECaNTc75Lz3b4k/TMJsoW46qoKFqGZWVTTj8Vx0uga/nRJwgILL8/OKRJl+5g==", 
+#   "customer"=>{"name"=>"Guido", "address"=>"Bali"}, 
+#   "commit"=>"Create Customer"
+# }
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
 
   # see all customers
-  get '/customers', to: 'customers#index'
-
+  get 'customers', to: 'customers#index'
+  # show the form to create new customer
+  get 'customers/new', to: 'customers#new', as: :new_customer
   # see one customer
-  get '/customers/:id', to: 'customers#show'
+  get 'customers/:id', to: 'customers#show', as: :customer
+
+  # show the edit form
+  get 'customers/:id/edit', to: 'customers#edit', as: :edit_customer
+  patch 'customers/:id', to: 'customer#update', as: :update_customer
+
+  # process the form
+
+  # process the form
+  post 'customers', to: 'customers#create', as: :create_customer
+
+  # login
+  get 'login', to: 'pages#login'
+
 end
